03a5e2a35c8648b5999538fc16e5e86f4da55ee4
igalshilman <igal@restate.dev>
2026-03-13 17:55:32 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs; mkShell {
+  name = "sdk-ruby";
+  buildInputs = [
+    # Ruby
+    ruby_3_3
+    bundler
+
+    # Rust toolchain
+    rustup
+    cargo
+    clang
+    llvmPackages.bintools
+
+    # Native build deps
+    pkg-config
+    openssl
+    libyaml
+    zlib
+  ];
+
+  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
+  shellHook = ''
+    export GEM_HOME="$PWD/.gem"
+    export PATH="$GEM_HOME/bin:$PATH"
+    export BUNDLE_PATH="$PWD/.bundle"
+  '';
+}
